12032005f30acac649b5c4a3043e3b8a83509295
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-03-13 05:36:56 +0100

    checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,8 @@
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
 
-print(f"a is {'lower' if islower('a') else 'upper'}")
-print(f"H is {'lower' if islower('H') else 'upper'}")
-print(f"A is {'lower' if islower('A') else 'upper'}")
-print(f"3 is {'lower' if islower('3') else 'upper'}")
-print(f"g is {'lower' if islower('g') else 'upper'}")
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
